a1ddfbae828476805ffe14f121d6f84117bda8cd
Yujin <jyooj08@naver.com>
2020-09-18 00:35:11 +0900

    Make simple multi-chatting program in python

---
+++ client.py
@@ -0,0 +1,39 @@
+from socket import *
+import threading
+
+serverIP = '192.168.0.13'
+serverPort = 12001
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((serverIP, serverPort))
+
+def getMessage():
+	global clientSocket
+	while True:
+		msg = clientSocket.recv(1024)
+		if not msg:
+			continue
+		print(msg.decode())
+
+
+#register client
+ID = input('Enter your ID: ')
+msg = "@register/"+ID
+clientSocket.send(msg.encode())
+
+t = threading.Thread(target=getMessage)
+t.daemon = True
+t.start()
+
+
+while True:
+	msg = input()
+	if msg == '@exit':
+		break	
+	msg = '@chat/'+ID+'/'+msg
+	clientSocket.send(msg.encode())
+
+msg = "@unregister/"+ID
+clientSocket.send(msg.encode())
+
+
+clientSocket.close()
---
+++ server.py
@@ -0,0 +1,47 @@
+from socket import *
+import threading
+
+serverPort = 12001
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind(('', serverPort))
+serverSocket.listen()
+
+client_list = []
+
+print('The TCP server is ready to receive')
+
+def addClient(clientSocket, addr):
+	global client_list
+	while True:
+		msg = clientSocket.recv(1024).decode()
+		
+		if not msg:
+			break
+		
+		print('msg: ',msg)
+		
+		data = msg.split('/')
+		if data[0] == '@register':
+			client_list.append(clientSocket)
+			#print(client_list,'\n')
+		elif data[0] == '@unregister':
+			client_list.remove(clientSocket)
+			#print(client_list,'\n')
+		elif data[0] == '@chat':
+			sendToAll(data[1], data[2])
+			
+		
+	clientSocket.close()
+	
+def sendToAll(ID, content):
+	global serverSocket, client_list
+	msg = ID + ": " + content
+	for clientSocket in client_list:
+		clientSocket.send(msg.encode())
+
+
+while True:
+	clientSocket, addr = serverSocket.accept()
+	t = threading.Thread(target=addClient, args=(clientSocket, addr))
+	t.start()
+	
